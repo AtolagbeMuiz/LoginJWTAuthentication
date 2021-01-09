cfbc2fe2cb60b7a34b0c236888a658d243d4f387
Atolagbe Muiz <atolagbemuiz@gmal.com>
2021-01-09 01:41:57 -0800

    Add project files.

---
+++ LoginJWTAuthentication/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginJWTAuthentication.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string EmailAddress { get; internal set; }
+    }
+}
